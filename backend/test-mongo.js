3122c88203c460b644cf93242b05562ae50fa3d6
Basavrajcharate <basavraj.charate130904@gmail.com>
2026-08-10 15:21:26 +0530

    mongo DB

--- backend/test-mongo.js
+++ backend/test-mongo.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-const uri = "mongodb://dmfmopwebsite_db_user:Dmfmop123@ac-ukwv50l-shard-00-00.8y6dqdo.mongodb.net:27017,ac-ukwv50l-shard-00-01.8y6dqdo.mongodb.net:27017,ac-ukwv50l-shard-00-02.8y6dqdo.mongodb.net:27017/?authSource=admin&replicaSet=atlas-jjlig9-shard-0&ssl=true&appName=Cluster0";
+const uri = process.env.MONGO_URI;
 
 mongoose.connect(uri)
   .then(() => {
